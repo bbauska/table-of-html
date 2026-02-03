1e9d9b3acc96db4928ac152d660fb3c55618e4b7
Brian Bauska <brianbauska@gmail.com>
2026-02-02 23:02:51 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* table-of-HTML/JS/scripts.js */
+Array.prototype.forEach.call(document.querySelectorAll('.key li'), function (li) {
+    li.addEventListener('mouseenter', function () {
+        document.body.classList.add('hover-' + this.className);
+    });
+    li.addEventListener('mouseleave', function () {
+        document.body.classList.remove('hover-' + this.className);
+    });
+});
